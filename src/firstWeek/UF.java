ac429888eb6e960bcabeb1c5ea3d3690a7aea645
kaminion <anysweetimes@naver.com>
2020-03-02 17:13:25 +0900

    [2020-03-02 17:13] Quick Union Add

--- src/firstWeek/UF.java
+++ src/firstWeek/UF.java
@@ -20,25 +20,37 @@ public class UF {
 
     }
 
+    // 루트 찾는 메서드
+    private int root(int i)
+    {
+        while( i != unionArray[i] ) i = unionArray[i]; // 값을 계속 추적해나가 루트를 구함.
+        return i;
+    }
+
     public void union(int p, int q)
     {
         // 근데 누구의 값을 덮어씔 건지 결정, 첫번째 인자에 두번째 인자껄 덮어씌움
         // 문제점 1. 첫번째 인자에 들어있는 값에 두번째 인자에 넣는 과정 필요
-        int pid = unionArray[p];
-        int qid = unionArray[q];
+//        int pid = unionArray[p];
+//        int qid = unionArray[q];
+//
+//        // 연결되어있는 모든것에 두번째 인자 삽입
+//        for(int i = 0; i < unionArray.length; i++)
+//        {
+//            if(unionArray[i] == pid) unionArray[i] = qid;
+//        }
 
-        // 연결되어있는 모든것에 두번째 인자 삽입
-        for(int i = 0; i < unionArray.length; i++)
-        {
-            if(unionArray[i] == pid) unionArray[i] = qid;
-        }
+        //QUICK UNION으로 변경
+        int i = root(p); // 한번에 바꾸어야하므로 이 트리 전체를 q로 받은 것에 붙인다.
+        int j = root(q); // 루트로 구분하므로 q의 루트를 구해서 붙인다.
+        unionArray[i] = j;
 
     }
 
     public boolean connected(int p, int q)
     {
-        // 같으면 true 대신 초기화가 됐다는 가정하에
-        return unionArray[p] == unionArray[q];
+        // 루트를 비교하면 됨
+        return root(p) == root(q);
     }
 
     public int find(int p)
